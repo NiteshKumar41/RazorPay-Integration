4e94113d65155194e00d8286ffe5c4f9cbf8530c
NiteshKumar41 <niteshkumar41@gmail.com>
2022-06-18 08:34:05 -0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from './auth-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'razorPay';
+  rzp1:any
+
+  constructor(private auth:AuthServiceService){
+
+  }
+  ngOnInit(){
+    
+  }
+   options = {
+    "key": "rzp_test_JiJ7Ibs8FFSkEl", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Acme Corp",
+    "description": "Test Transaction",
+    "image": "https://example.com/your_logo",
+    "order_id": "", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+    "prefill": {
+        "name": "Gaurav Kumar",
+        "email": "gaurav.kumar@example.com",
+        "contact": "9999999999"
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+  pay(){
+  this.rzp1 = new this.auth.nativeWindow.Razorpay(this.options);
+  this.rzp1.open();
+  }
 }
